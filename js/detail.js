43973696a8e7c9464200109f5792d6c3bf8639b0
hll <347441884@qq.com>
2017-11-28 19:44:57 +0800

    sui练习

---
+++ js/detail.js
@@ -0,0 +1,10 @@
+/**
+ * Created by chen on 2017/8/20.
+ */
+$(".mask").click(function(){
+    $(this).css({"display":"none"});
+});
+
+$(function(){
+    alert(1);
+});
---
+++ js/sort.js
@@ -0,0 +1,7 @@
+/**
+ * Created by chen on 2017/8/18.
+ */
+
+    $(".list-block").click(function(){
+        $.router.load("detail.html");
+    });
